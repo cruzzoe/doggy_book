ab73d1d580622a916a17d78d6cc479b6b0479926
cruz <mike.robintino@gmail.com>
2021-03-19 19:04:22 +0000

    Fix dotenv loading

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ADMINS = ['michael.laurence.robinson@gmail.com']
+    ADMINS = os.environ.get('ADMINS')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_USE_SSL = False
     MAIL_USE_TLS = True
