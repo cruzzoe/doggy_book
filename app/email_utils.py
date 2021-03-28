8c79a4356bee102af9662d7d9511c3f0c37385fd
cruz <mike.robintino@gmail.com>
2021-03-28 12:03:00 +0100

    Re-enable emails!

--- app/email_utils.py
+++ app/email_utils.py
@@ -13,7 +13,7 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body, attachment_path=None):
-    if app.debug:
+    if not app.debug:
         msg = Message(subject, sender=sender, recipients=recipients)
         msg.body = text_body
         msg.html = html_body
